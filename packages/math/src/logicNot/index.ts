841b846a0039055755b13d65ba6eec6f0dc57f12
Toby Nguyen <tobynguyen27@proton.me>
2026-05-15 22:21:38 +0700

    chore(lint): disable rules (#3)

--- packages/math/src/logicNot/index.ts
+++ packages/math/src/logicNot/index.ts
@@ -2,6 +2,5 @@ import { useComputed$ } from '@builder.io/qwik';
 import { MaybeSignal, toValue } from '@qwikgear/shared';
 
 export function logicNot(value: MaybeSignal<any>) {
-	// oxlint-disable-next-line qwik/use-method-usage this is a hook but not start with "use"
 	return useComputed$(() => !toValue(value));
 }
